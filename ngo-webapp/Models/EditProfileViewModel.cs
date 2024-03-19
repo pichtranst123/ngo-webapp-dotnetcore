6b14d7d9270f71d2b79be00bd111f419e7a38fa8
Nguyen Le Quang Thai <quangthai0997@gmail.com>
2024-03-19 22:51:08 +0700

    Staged codes

---
+++ ngo-webapp/Models/EditProfileViewModel.cs
@@ -0,0 +1,8 @@
+namespace ngo_webapp.Models;
+
+public class EditProfileViewModel
+{
+    public string Username { get; set; }
+    public string User_Image { get; set; }
+    public string Bio { get; set; }
+}
