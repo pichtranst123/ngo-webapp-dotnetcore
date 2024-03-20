591d1b117106bf64b8be8cd2b8b7edd3f801796a
Hiếu Minh <129259600+HieuMinh1309@users.noreply.github.com>
2024-03-20 13:52:27 +0700

    UI: Export Bill interface

--- ngo-webapp/Models/Entities/NgoManagementContext.cs
+++ ngo-webapp/Models/Entities/NgoManagementContext.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,15 +23,15 @@ public partial class NgoManagementContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
         => optionsBuilder.UseSqlServer("Data Source=DEVBLOCK;Initial Catalog=NGO-Management;Persist Security Info=True;User ID=sa;Password=05012004;Trust Server Certificate=True");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Appeal>(entity =>
         {
-
-            entity.HasKey(e => e.AppealsId).HasName("PK__Appeals__5E813B30A8132DFC");
+            entity.HasKey(e => e.AppealsId).HasName("PK__Appeals__5E813B3049020FB9");
 
             entity.HasIndex(e => e.AppealsName, "UC_Appeals_AppealsName").IsUnique();
 
@@ -54,8 +54,8 @@ public partial class NgoManagementContext : DbContext
 
         modelBuilder.Entity<Blog>(entity =>
         {
+            entity.HasKey(e => e.BlogId).HasName("PK__Blogs__54379E508A296491");
 
-            entity.HasKey(e => e.BlogId).HasName("PK__Blogs__54379E50C6BFD400");
             entity.Property(e => e.BlogId).HasColumnName("BlogID");
             entity.Property(e => e.AppealId).HasColumnName("AppealID");
             entity.Property(e => e.Content).IsUnicode(false);
@@ -72,14 +72,12 @@ public partial class NgoManagementContext : DbContext
 
             entity.HasOne(d => d.User).WithMany(p => p.Blogs)
                 .HasForeignKey(d => d.UserId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Blogs_Users");
         });
 
         modelBuilder.Entity<Donation>(entity =>
         {
-            entity.HasKey(e => e.DonationId).HasName("PK__Donation__C5082EDB745B0277");
-
+            entity.HasKey(e => e.DonationId).HasName("PK__Donation__C5082EDB43EA259E");
 
             entity.Property(e => e.DonationId).HasColumnName("DonationID");
             entity.Property(e => e.Amount).HasColumnType("decimal(10, 2)");
@@ -98,17 +96,14 @@ public partial class NgoManagementContext : DbContext
 
         modelBuilder.Entity<User>(entity =>
         {
-            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CCAC6A9A6DB9");
-
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CCAC7F82A773");
 
             entity.HasIndex(e => e.Email, "UC_Users_Email").IsUnique();
 
             entity.HasIndex(e => e.Username, "UC_Users_Username").IsUnique();
 
             entity.Property(e => e.UserId).HasColumnName("UserID");
-            entity.Property(e => e.Balance)
-                .HasDefaultValue(1000m)
-                .HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Balance).HasColumnType("decimal(10, 2)");
             entity.Property(e => e.Email)
                 .HasMaxLength(100)
                 .IsUnicode(false);
